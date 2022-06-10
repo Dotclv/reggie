b9551951dd823a5b93252134a1465e810d9490d2
Dotclv <yumingyang0212@163.com>
2022-06-10 22:17:04 +0800

    Initial commit

---
+++ src/main/java/com/mingyang/reggie/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package com.mingyang.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author: ymy
+ * @program: reggie
+ * @description:
+ * @date: 2022/6/10 22:04
+ * @version: 1.0
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射");
+        registry.addResourceHandler("/backend/**")
+                .addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**")
+                .addResourceLocations("classpath:/front/");
+    }
+}

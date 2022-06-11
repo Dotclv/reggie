4770e317cff15558d069f2ac53d0e2d29e7d5eeb
Dotclv <yumingyang0212@163.com>
2022-06-11 17:05:35 +0800

    添加拦截器 拦截前端请求

--- src/main/java/com/mingyang/reggie/ReggieApplication.java
+++ src/main/java/com/mingyang/reggie/ReggieApplication.java
@@ -3,9 +3,11 @@ package com.mingyang.reggie;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @MapperScan("com.mingyang.reggie.mapper")
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
 
     public static void main(String[] args) {
